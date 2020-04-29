9b1af40822e2e4b6ae3fc9a2ef64f52a42c28f88
DerpUsesPython <64509987+DerpUsesPython@users.noreply.github.com>
2020-04-29 12:59:05 -0700

    Add files via upload

---
+++ CHATBOT.py
@@ -0,0 +1,193 @@
+true = 1
+import secrets
+import time
+while true == 1:
+    print ('Hello. This is a chatbot for talking. Enter in text to get a response. Some responses have not been coded in yet, please send ideas!')
+    print ('This has been coded to go on forever. Please exit the program when you want to stop.')
+    PlayerInput = input()
+    if PlayerInput == 'ERROR':
+        print ('ERROR CODE 001: INTENDED ERROR')
+    else:
+        if PlayerInput == 'Generate a random_fruit':
+            RandomFruit =('apple','banana','pear','aisian pear','orange','strawberry','blueberry','blackberry','rasberry')
+            print(secrets.choice(RandomFruit))
+        else:
+            if PlayerInput == 'Hello':
+                HiHello = ('hi!','Hello!')
+                print (secrets.choice(HiHello))
+            else:
+                if PlayerInput == 'Alt+f4':
+                    print ('Press The X button.')
+                else:
+                    if PlayerInput == 'I want a banana':
+                        print ('Sure, have a banana.')
+                    else:
+                        if PlayerInput == 'I will spank you':
+                            print ('no')
+                        else:
+                            if PlayerInput == 'Step on Lego':
+                                print ('ERROR CODE 002: Chatbot.py stepped on lego')
+                                time.sleep(3)
+                                print ('Error fixed.')
+                            else:
+                                 if PlayerInput == 'Do you know da wae?':
+                                     print ('I thought that meme was dead...')
+                                 else:
+                                    if PlayerInput == 'I cause torture':
+                                        print ('Oh hello, GrayStillPlays')
+                                    else:
+                                        if PlayerInput == 'Write me a haiku':
+                                            print ('Okay, it will be bad though.')
+                                            RandomHaiku1 = ('Peanut','Brain','Organ','Sad','Glad','Mad','Red','Blue','Me','I')
+                                            RandomHaiku2 = ('Am','Was','Is','Had','How')
+                                            RandomHaiku3 = ('A','Glad','Mad','Sad','Not')
+                                            RandomHaiku4 = ('Robot','Happy','Sunshine','kitten')
+                                            RandomHaiku5 = ('And','So','But')
+                                            RandomHaiku6 = ('I', 'He', 'She','They')
+                                            RandomHaiku7 = ('Helped','Ran','Swam','Drove')
+                                            RandomHaiku8 = ('Across','A Friend','On','Over')
+                                            RandomHaiku9 = ('A bridge','A path','Get Milk' 'A river' 'Back home')
+                                            RandomHaiku10 = ('Then','And')
+                                            RandomHaiku11 = ('Played','Jumped','Sat')
+                                            RandomHaiku12 = ('Down','High','A')
+                                            RandomHaiku13 = ('Game,','And')
+                                            RandomHaiku14 = ('Ate','Fun!')
+                                            Haiku1 = (secrets.choice(RandomHaiku1))
+                                            Haiku2 = (secrets.choice(RandomHaiku2))
+                                            Haiku3 = (secrets.choice(RandomHaiku3))
+                                            Haiku4 = (secrets.choice(RandomHaiku4))
+                                            Haiku5 = (secrets.choice(RandomHaiku5))
+                                            Haiku6 = (secrets.choice(RandomHaiku6))
+                                            Haiku7 = (secrets.choice(RandomHaiku7))
+                                            Haiku8 = (secrets.choice(RandomHaiku8))
+                                            Haiku9 = (secrets.choice(RandomHaiku9))
+                                            Haiku10 = (secrets.choice(RandomHaiku10))
+                                            Haiku11 = (secrets.choice(RandomHaiku11))
+                                            Haiku12 = (secrets.choice(RandomHaiku12))
+                                            Haiku13 = (secrets.choice(RandomHaiku13))
+                                            Haiku14 = (secrets.choice(RandomHaiku14))
+                                            print (Haiku1,Haiku2,Haiku3,Haiku4,',',Haiku5,Haiku6,Haiku7,Haiku8,Haiku9,',',Haiku10,Haiku11,Haiku12,Haiku13,Haiku14)
+                                            print ('I hope you enjoyed.')
+                                        else:
+                                            if PlayerInput == 'I want a banana error':
+                                                print ('When i repeatedly forgot to make the I want a banana query a string while making a response.')
+                                            else:
+                                                if PlayerInput == 'How are you today?':
+                                                    Emotion = ('Happy','Amazing','Good','Great','OK','Not the best','Badly','Well','Not too bad','Awful...')
+                                                    print (secrets.choice(Emotion))
+                                                else:
+                                                    if PlayerInput == 'Fortnite good minecraft bad':
+                                                        print ('WRONG!')
+                                                    else:
+                                                        if PlayerInput == 'Mincraft good fortnite bad':
+                                                            print ('Yes.')
+                                                        else:
+                                                            if PlayerInput == 'Number Guess game':
+                                                                print ('I will think of a random number between 1 and 10. You must guess the number.')
+                                                                NumberGuess = ('1','2','3','4','5','6','7','8','9','10')
+                                                                NumberGuessChoice = (secrets.choice(NumberGuess))
+                                                                PlayerInputNumberGuess = input()
+                                                                if PlayerInputNumberGuess == NumberGuessChoice:
+                                                                    print ('Correct!')
+                                                                else:
+                                                                    print ('Sorry, the correct answer is',NumberGuessChoice)
+                                                            else:
+                                                                print ('ERROR CODE 003: THis has not been coded yet.')
+                                                        
+            
+    true = 2
+while true == 2:
+    print ('Please enter another query.')
+    true = 0
+    PlayerInput2 = input()
+    if PlayerInput2 == 'ERROR':
+        print ('ERROR CODE 001: INTENDED ERROR')
+    else:
+        if PlayerInput2 == 'Generate a random fruit':
+            RandomFruit =('apple','banana','pear','aisian pear','orange','strawberry','blueberry','blackberry','rasberry')
+            print(secrets.choice(RandomFruit))
+        else:
+            if PlayerInput2 == 'Hello':
+                Hiorhello = ('hi!','Hello!')
+                print (secrets.choice(Hiorhello))
+            else:
+                if PlayerInput2 == 'Alt+f4':
+                    print ('Press The X button.')
+                else:
+                    if PlayerInput2 == 'I want a banana':
+                        print ('Sure, have a banana.')
+                    else:
+                        if PlayerInput2 == 'I will spank you':
+                            print ('no')
+                        else:
+                            if PlayerInput2 == 'Step on Lego':
+                                print ('ERROR CODE 002: Chatbot.py stepped on lego')
+                                time.sleep(3)
+                                print ('Error fixed.')
+                            else:
+                                if PlayerInput2 == 'Do you know da wae?':
+                                    print ('I thought that meme was dead...')
+                                else:
+                                    if PlayerInput2 == 'I cause torture':
+                                        print ('Oh hello, GrayStillPlays')
+                                    else:
+                                        if PlayerInput2 == 'Write me a haiku':
+                                            print ('Okay, it will be bad though.')
+                                            RandomHaiku1 = ('Peanut','Brain','Organ','Sad','Glad','Mad','Red','Blue','Me','I')
+                                            RandomHaiku2 = ('Am','Was','Is','Had','How')
+                                            RandomHaiku3 = ('A','Glad','Mad','Sad','Funny','Not')
+                                            RandomHaiku4 = ('Robot','Happy','Sunshine','kitten')
+                                            RandomHaiku5 = ('And','So','But')
+                                            RandomHaiku6 = ('I', 'He', 'She','They')
+                                            RandomHaiku7 = ('Helped','Ran','Swam','Drove')
+                                            RandomHaiku8 = ('Across','A Friend','On','Over')
+                                            RandomHaiku9 = ('A bridge','A path','Get Milk' 'A river' 'Back home')
+                                            RandomHaiku10 = ('Then','And')
+                                            RandomHaiku11 = ('Played','Jumped','Sat')
+                                            RandomHaiku12 = ('Down','High','A')
+                                            RandomHaiku13 = ('Game,','And')
+                                            RandomHaiku14 = ('Ate','Fun!')
+                                            Haiku1 = (secrets.choice(RandomHaiku1))
+                                            Haiku2 = (secrets.choice(RandomHaiku2))
+                                            Haiku3 = (secrets.choice(RandomHaiku3))
+                                            Haiku4 = (secrets.choice(RandomHaiku4))
+                                            Haiku5 = (secrets.choice(RandomHaiku5))
+                                            Haiku6 = (secrets.choice(RandomHaiku6))
+                                            Haiku7 = (secrets.choice(RandomHaiku7))
+                                            Haiku8 = (secrets.choice(RandomHaiku8))
+                                            Haiku9 = (secrets.choice(RandomHaiku9))
+                                            Haiku10 = (secrets.choice(RandomHaiku10))
+                                            Haiku11 = (secrets.choice(RandomHaiku11))
+                                            Haiku12 = (secrets.choice(RandomHaiku12))
+                                            Haiku13 = (secrets.choice(RandomHaiku13))
+                                            Haiku14 = (secrets.choice(RandomHaiku14))
+                                            print (Haiku1,Haiku2,Haiku3,Haiku4,',',Haiku5,Haiku6,Haiku7,Haiku8,Haiku9,',',Haiku10,Haiku11,Haiku12,Haiku13,Haiku14)
+                                            print ('I hope you enjoyed.')
+                                        else:
+                                            if PlayerInput2 == 'I want a banana error':
+                                                print ('When i repeatedly forgot to make the I want a banana query a string while making a response.')
+                                            else:
+                                                if PlayerInput2 == 'How are you today?':
+                                                    Emotion = ('Happy','Amazing','Good','Great','OK','Not the best','Badly','Well','Not too bad','Awful...')
+                                                    print (secrets.choice(Emotion))
+                                                else:
+                                                    if PlayerInput2 == 'Fortnite good minecraft bad':
+                                                        print ('WRONG!')
+                                                    else:
+                                                        if PlayerInput2 == 'Mincraft good fortnite bad':
+                                                            print ('Yes.')
+                                                        else:
+                                                            if PlayerInput2 == 'Number Guess game':
+                                                                print ('I will think of a random number between 1 and 10. You must guess the number.')
+                                                                NumberGuess = ('1','2','3','4','5','6','7','8','9','10')
+                                                                NumberGuessChoice = (secrets.choice(NumberGuess))
+                                                                PlayerInputNumberGuess = input()
+                                                                if PlayerInputNumberGuess == NumberGuessChoice:
+                                                                    print ('Correct!')
+                                                                else:
+                                                                    print ('Sorry, the correct answer is',NumberGuessChoice)
+                                                            else:
+                                                                print ('ERROR CODE 003: THis has not been coded yet.')
+                                                        
+                                                        
+    true = 2
